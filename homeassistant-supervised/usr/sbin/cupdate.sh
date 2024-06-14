bc302b909eb4a689b0837b5daa7bd5f5139aa041
guxiaoqi2007 <243757555@qq.com>
2024-06-14 13:50:03 +0800

    Update cupdate.sh

--- homeassistant-supervised/usr/sbin/cupdate.sh
+++ homeassistant-supervised/usr/sbin/cupdate.sh
@@ -4,6 +4,11 @@ echo "$tmp"
 if [ -n "$tmp" ]; then
 sed -i 's/URL_HASSIO_VERSION = \"https:\/\/version.home-assistant.io\/{channel}.json\"/URL_HASSIO_VERSION = \"http:\/\/guxiaoqi.dynv6.net:888\/{channel}.json\"/' $tmp
 fi
+tmp2=$(find /var/lib/docker/overlay2 -path '*diff/usr/src/supervisor/supervisor/homeassistant/module.py')
+echo "$tmp2"
+if [ -n "$tmp2" ]; then
+sed -i 's|ghcr.io/home-assistant/|registry.cn-hangzhou.aliyuncs.com/gxq2007/|' $tmp2
+fi
 #sed -i "s/= data\[\"images\"\]\[\"core\"\]/= str(data[\"images\"][\"core\"]).replace(\'.io\', \'.nju.edu.cn\')/g" $tmp
 #sed -i "s/= data\[\"images\"\]\[\"supervisor\"\]/= str(data[\"images\"][\"supervisor\"]).replace(\'.io\', \'.nju.edu.cn\')/g" $tmp
 #sed -i "s/= data\[\"images\"\]\[\"audio\"\]/= str(data[\"images\"][\"audio\"]).replace(\'.io\', \'.nju.edu.cn\')/g" $tmp
