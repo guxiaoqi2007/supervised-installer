11776c191f37ede0f85396b70ff450b65c9f5ec7
guxiaoqi2007 <243757555@qq.com>
2025-01-14 09:28:20 +0800

    Update cupdate.sh

--- homeassistant-supervised/usr/sbin/cupdate.sh
+++ homeassistant-supervised/usr/sbin/cupdate.sh
@@ -18,3 +18,37 @@ fi
 #sed -i "s/= data\[\"images\"\]\[\"multicast\"\]/= str(data[\"images\"][\"multicast\"]).replace(\'.io\', \'.nju.edu.cn\')/g" $tmp
 #sed -i "s/.io/.dockerproxy.com/g" /usr/share/hassio/addons/git/a0d7b954/.addons.yml
 
+# 查找文件
+file_path=$(find /var/lib/docker/overlay2 -path '*diff/usr/src/supervisor/supervisor/docker/interface.py' -print -quit)
+file_path1=$(find /var/lib/docker/overlay2 -path '*diff/usr/src/supervisor/supervisor/docker/manager.py' -print -quit)
+# 定义行号和要插入的文本
+line_number=234
+line_number1=192
+insert_text='        image = image.replace("homeassistant", "registry.cn-hangzhou.aliyuncs.com/gxq2007", 1)'
+
+# 检查文件是否存在
+if [ -n "$file_path" ]; then
+    if ! grep -qF -- "$insert_text" "$file_path"; then
+        sed -i "${line_number}i\\${insert_text}" "$file_path"
+        echo "已成功插入到第${line_number}行。"
+    else
+        echo "第${line_number}行已包含相同的文本，无需插入。"
+    fi
+else
+    echo "未找到文件。"
+fi
+
+if [ -n "$file_path1" ]; then
+    if ! grep -qF -- "$insert_text" "$file_path1"; then
+        sed -i "${line_number1}i\\${insert_text}" "$file_path1"
+        echo "已成功插入到第${line_number1}行。"
+    else
+        echo "第${line_number1}行已包含相同的文本，无需插入。"
+    fi
+else
+    echo "未找到文件。"
+fi
+
+
+
+
